f43c7ab2fac993db3c6aa548597a589ac3593554
crayon356 <75258955+crayon356@users.noreply.github.com>
2021-02-27 21:34:27 +0900

    ユーザー管理機能実装_再々修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,10 +5,17 @@ class User < ApplicationRecord
   with_options presence: true do
     validates :nickname
     validates :email, uniqueness: { case_sensitive: true }
-    validates :family_name, format: { with: /\A[ぁ-んァ-ン一-龥]/ }
-    validates :first_name, format: { with: /\A[ぁ-んァ-ン一-龥]/ }
-    validates :family_name_kana, format: { with: /\A[ァ-ヶーー]+\z/ }
-    validates :first_name_kana, format: { with: /\A[ァ-ヶーー]+\z/ }
+    
+    with_options format: { with: /\A[ぁ-んァ-ン一-龥]/ } do
+      validates :family_name
+      validates :first_name
+    end
+    
+    with_options format: { with: /\A[ァ-ヶーー]+\z/ } do
+      validates :family_name_kana
+      validates :first_name_kana
+    end
+    
     validates :birthday
   end
     PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
