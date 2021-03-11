515f1ba032ce2cb1df6c173636097e25f6498123
crayon356 <75258955+crayon356@users.noreply.github.com>
2021-03-11 19:25:58 +0900

    商品情報編集実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,9 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, only: [:new, :create]
-  
+  before_action :authenticate_user!, only: [:new, :create, :edit, :update]
+  before_action :move_to_edit, only: [:edit]
+  # 購入機能実装後に確認すること
+  #before_action :soldout_to_edit, only: [:edit]
+
   def index
     @items = Item.order('created_at DESC')
   end
@@ -22,9 +25,36 @@ class ItemsController < ApplicationController
     @item = Item.find(params[:id])
   end
 
+  def edit
+    @item = Item.find(params[:id])
+  end
+
+  def update
+    @item = Item.find(params[:id])
+    if @item.update(item_params)
+        redirect_to item_path
+      else
+        render :edit
+      end
+  end
+
   private
     def item_params
       params.require(:item).permit(:image, :name, :category_id, :state_id, :province_id, :souryou_id, :day_id, :price, :comment).merge(user_id: current_user.id)
     end
+
+    def move_to_edit
+      @item = Item.find(params[:id])
+      unless current_user.id == @item.user_id
+        redirect_to root_path
+      end
+    end
+    #購入機能実装後に確認
+    #def soldout_to_edit
+      #@item = Item.find(params[:id])
+      #unless buyer_id.empty?
+        #redirect_to root_path
+      #end
+    #end
 end
 
